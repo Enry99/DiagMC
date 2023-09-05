28b4069c07f84fbb994bd765fb2e775c25bc105d
Enry99 <Enry99@users.noreply.github.com>
2023-09-05 17:27:18 +0000

    deploy: 242118d7f6d44ef9b72a52ce666627fa169efb03

--- search/all_3.js
+++ search/all_3.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['diagmc_0',['DiagMC',['../index.html',1,'']]],
-  ['diagram_1',['Diagram',['../classDiagram.html',1,'Diagram'],['../classDiagram.html#a316f2ad350bc0b8d5dcc3024a69fbc6d',1,'Diagram::Diagram()']]],
-  ['diagram_2eh_2',['diagram.h',['../diagram_8h.html',1,'']]],
-  ['diagram_5fcore_3',['Diagram_core',['../classDiagram__core.html',1,'Diagram_core'],['../classDiagram__core.html#a4639a70b020a2d08bc473cafd00297b3',1,'Diagram_core::Diagram_core()']]]
+  ['diagram_0',['Diagram',['../classDiagram.html',1,'Diagram'],['../classDiagram.html#a316f2ad350bc0b8d5dcc3024a69fbc6d',1,'Diagram::Diagram()']]],
+  ['diagram_2eh_1',['diagram.h',['../diagram_8h.html',1,'']]],
+  ['diagram_5fcore_2',['Diagram_core',['../classDiagram__core.html',1,'Diagram_core'],['../classDiagram__core.html#a4639a70b020a2d08bc473cafd00297b3',1,'Diagram_core::Diagram_core()']]],
+  ['diagrammatic_20monte_20carlo_20simulation_20of_20a_202_2dlevel_20spin_20system_20in_20an_20external_20magnetic_20field_3',['Diagrammatic Monte Carlo simulation of a 2-level spin system in an external magnetic field',['../index.html',1,'']]]
 ];
--- search/pages_0.js
+++ search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['diagmc_0',['DiagMC',['../index.html',1,'']]]
+  ['diagrammatic_20monte_20carlo_20simulation_20of_20a_202_2dlevel_20spin_20system_20in_20an_20external_20magnetic_20field_0',['Diagrammatic Monte Carlo simulation of a 2-level spin system in an external magnetic field',['../index.html',1,'']]]
 ];
